ba4e5e911d17bdf47e132346abd127c361709bc6
Jeroen <xnassist@gmail.com>
2017-09-11 00:41:52 +0200

    Add files via upload
    Beta interface

--- components/scripts/hotmail.js
+++ components/scripts/hotmail.js
@@ -1,6 +1,9 @@
 /***********************************************************
 Live(Hotmail)
 ***********************************************************/
+var name="Hotmail";
+var ver="2017-09-10";
+
 var hostString="hotmail.com";
 var supportInboxOnly=true;
 var supportShowFolders=true;
@@ -11,41 +14,84 @@ function init(){
   this.loginData=["","login","passwd","KMSI=1&LoginOptions=1"];
   this.dataURL="https://mail.live.com/default.aspx?rru=inbox";
   this.viewURL="https://mail.live.com/default.aspx?rru=inbox";
-  this.viewDomain="mail.live.com";
+  this.viewDomain="outlook.live.com";
+  if(this.password.length>16)this.password=this.password.substring(0,16);
+
+  this.cookieDomain="live.com";
+
+  this.logoutURL="https://login.live.com/logout.srf";
 }
 function getIconURL(){
-  return "http://a.gfx.ms/OLFav.ico";
+  return "https://outlook.live.com/mail/favicon.ico";
 }
-function checkLogin(aData,aHttp){
+function checkLogin(aData){
   switch(this.stage){
   case ST_CHECK:
+    if(this.cookies){
+      this.stage=ST_DATA;
+      this.setCookies();
+      return true;
+    }
     this.getHtml(this.viewURL);
     return false;
   case ST_CHECK+1:
     var fnd=aData.match(/urlPost:/);
-    var fnd2=aData.match(/sFT:'(\S+?)'/);
-    if(!fnd||fnd2){//logged in
-      this.stage=ST_LOGIN_RES;
-      return this.process(aData,aHttp);
+    if(!fnd){
+      var reg=new RegExp("\"Address\":\""+this.user.replace(/@/,"\\\\u0040")+"\"","i");
+      var fnd2=aData.match(reg);
+      if(fnd2){//logged in already
+        this.stage=ST_LOGIN_RES;
+        return this.process(aData);
+      }else{
+        var fnd3=aData.match(/clientId:\s*?'(\S+?)'/);
+        var fnd4=aData.match(/window\.clientId/);
+        if(!fnd3&&!fnd4){
+          this.cookieManager.clear();
+          this.stage=this.initStage;
+          this.getHtml(this.logoutURL);
+          return true;
+        }else{
+          if(fnd3)this.viewURL="https://outlook.live.com/mail/";
+          else this.viewURL="https://outlook.live.com/owa/#path=/mail";
+          this.dataURL=["https://outlook.live.com/owa/sessiondata.ashx?appcacheclient=0","appcacheclient=0"];
+          this.getHtml(this.dataURL);
+          return false;
+        }
+      }
     }else{
+      var fnd=aData.match(/sFT:'(\S+?)'/);
+      if(fnd)this.stage=ST_LOGIN_RES;
+      else this.stage=ST_PRE_RES;
+      return this.process(aData);
+    }
+  case ST_CHECK+2:
+    var f=aData.match(new RegExp('"LogonEmailAddress":"'+this.user+'"',"i"));
+    if(f){
+      this.isNew=2;
+      this.stage=ST_DATA_RES;
+      return this.process(aData);      
+    }else{
+      this.viewURL="https://mail.live.com/default.aspx?rru=inbox";
       this.cookieManager.clear();
       this.stage=this.initStage;
-      return this.process("");
+      this.getHtml(this.logoutURL);
+      return true;      
     }
   }
   this.onError();
   return true;
 }
-function process(aData,aHttp){
-//dout(this.user+" "+this.stage);
+function process(aData,aHttp) {  
 if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
   switch(this.stage){
   case ST_PRE:
+// vs 3 cookieManager code
     try{
       var s=this.main.prefBranch.getCharPref("accounts.["+this.id+"#"+this.user+"].cookie");
       s=s.split("\t");
       this.cookieManager.addCookies(s[0],s[1]);
     }catch(e){}
+// END vs 3 cookieManager code
     this.getHtml("https://mail.live.com");
     return false;
   case ST_PRE_RES:
@@ -66,7 +112,7 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
     if(fnd){//2-step verification
       var fnd2=aData.match(/urlPost:'(\S+?)'/);
       var fnd3=aData.match(/data:'([^}]+?)',type:([^}]+?),[^}]*?display:'([^}]+?)',[^}]*?otcSent:([^,]+?),[^}]*?isSADef:true,isVoiceDef:(\S+?)[,}]/);
-      var fnd5=aData.match(/,G:'(\S+?)'/);
+      var fnd5=aData.match(/,g:'(\S+?)'/);
       if(fnd2&&fnd3&&fnd5){
         this.form=[fnd2[1],
                   "login="+encodeURIComponent(fnd5[1])+"&type="+(fnd3[2]=="10"?19:18)
@@ -100,7 +146,7 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
             case type.AltEmail:
               dtype="AltEmail";
               break;
-            }       
+            }
             this.authURL=[fnd4[1]+"GetOneTimeCode.srf",
                         "login="+encodeURIComponent(fnd5[1])+"&flowtoken="+encodeURIComponent(fnd[1])+"&purpose=eOTT_OneTimePassword&channel="+chn
                         +(dtype?"&"+dtype+"="+encodeURIComponent(fnd3[1]):"")+"&UIMode=11"];            
@@ -126,14 +172,30 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
     }
     ++this.stage;
   case ST_LOGIN_RES+1:
-    var fnd=aData.match(/window.location.replace\([\'\"](\S+?)[\'\"]/);
+    var fnd=aData.match(/window.location.replace\([\'\"](\S+?)[\'\"]/);      
     if(fnd){//login expired
       this.getHtml(fnd[1]);
       return false;
     }
     ++this.stage;
   case ST_LOGIN_RES+2:
-    var fnd=aData.match(/window\.clientId/);
+    var f=this.getForm(aData,"fmHF",true);
+    if(f){//new outlook UI
+      this.getHtml(f);
+      return false;
+    }    
+    ++this.stage;
+  case ST_LOGIN_RES+3:
+    var fnd=aData.match(/clientId:\s*?'(\S+?)'/);//new beta  
+    if(fnd){
+      this.isNew=2;
+      this.dataURL=["https://outlook.live.com/owa/sessiondata.ashx?appcacheclient=0","appcacheclient=0"];
+      this.viewURL="https://outlook.live.com/mail/";
+      this.mailHost=this.viewURL;
+      this.stage=ST_DATA;
+      break;
+    }    
+    fnd=aData.match(/window\.clientId/);
     if(fnd){
       this.isNew=2;
       this.dataURL=["https://outlook.live.com/owa/sessiondata.ashx?appcacheclient=0","appcacheclient=0"];
@@ -160,33 +222,37 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
     if(fnd){
       fnd=unescape(fnd[1].replace(/\\u/g,"%u"));
       this.dataURL=fnd;
+      this.viewURL=fnd;
       var url=this.dataURL.match(/(((\S+):\/\/([^/]+))(\S*\/)?)([^/]*)/);
       if(url)this.mailHost=url[2];
       this.stage=ST_DATA_RES;
     }else{
-      fnd=aData.match(/BrowserSupport.aspx[\s\S]+?BrowserSupport.aspx[\s\S]+?(BrowserSupport.aspx\S+?)"/);
+      fnd=aData.match(/href="(\S+?InboxLight.aspx\S+?)">/);
+      if(fnd){//Thunderbird(browser check)
+        fnd=fnd[1].replace(/&#(\d+);/g,function(){return String.fromCharCode(RegExp.$1);});
+        this.stage=ST_LOGIN_RES+4;
+        this.getHtml(fnd);
+        return true;
+      }
+      fnd=aData.match(/"hn":"(\S+?)"/);//Https check
       if(fnd){
-        var fnd2=aData.match(/"hn":"(\S+?)"/);
-        if(fnd2){
-          fnd2="https://"+fnd2[1];
-          this.mailHost=fnd2;
-          this.stage=ST_LOGIN_RES+3;
-          this.getHtml(this.mailHost);
-          return true;
-        }
+        this.dataURL="https://"+fnd[1]+"/default.aspx?rru=inbox";
+        this.mailHost="https://"+fnd[1];
+        this.viewURL=this.dataURL;
+        this.stage=ST_DATA;
       }
     }
     break;
-  case ST_LOGIN_RES+3:
-    var fnd=aData.match(/href="(\/m\/folders\.m\/list)"/);
-    if(fnd){//FF 3.x
-      this.mobile=true;
-      this.dataURL=this.mailHost+fnd[1];
-      this.stage=ST_DATA;
-    }else this.stage=ST_DATA_RES;
+  case ST_LOGIN_RES+4:
+    var fnd=aData.match(/<base\s+href="(\S+?)"/);
+    if(fnd){
+      var url=fnd[1].match(/(((\S+):\/\/([^/]+))(\S*\/)?)([^/]*)/);
+      if(url)this.mailHost=url[2];
+    }
+    this.stage=ST_DATA_RES;
     break;
   case (ST_LOGIN_RES+5)://2-step verification
-    if(aData.match(/"State":201/)||aData.match(/"State":204/)){
+    if(aData.match(/"State":201/)){
       this.stage=(ST_LOGIN_RES+6);
       this.openAuthDialog(this.id,this.user,null);
       return true;
@@ -194,7 +260,7 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
     break;
   case (ST_LOGIN_RES+6)://2-step verification
     if(aData){
-      this.stage=ST_LOGIN_RES+10;
+      this.stage=ST_LOGIN_RES+10;// vs 3
       this.getHtml(this.form[0],"otc="+encodeURIComponent(aData)+"&AddTD=1&"+this.form[1]);
       delete this.form;
       return true;
@@ -207,7 +273,8 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
       delete this.authURL;
       return true;
     }
-    break;
+    break;    
+// vs 3 cookieManager code
   case (ST_LOGIN_RES+10)://2-step verification
     this.stage=ST_LOGIN_RES;
     var ck=this.cookieManager.findCookieString("login.live.com","SDIDC");
@@ -215,6 +282,7 @@ if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
       this.main.prefBranch.setCharPref("accounts.["+this.id+"#"+this.user+"].cookie",aHttp.URI.spec+"\t"+ck);
     }
     return this.process(aData,aHttp);
+// END vs 3 cookieManager code
   }
   return this.baseProcess(aData,aHttp);
 }
@@ -222,10 +290,15 @@ function getData(aData){
   var obj={};
   var ar=[];
   this.count=-1;
-  
+
   if(this.isNew==2){
     try{
       var o=JSON.parse(aData);
+
+      var reg=new RegExp(this.user,"i");
+      var fnd2=o.owaUserConfig.SessionSettings.LogonEmailAddress.match(reg);
+      if(!fnd2)return obj;
+
       var l=o.findFolders.Body.ResponseMessages.Items[0].RootFolder.Folders;
       this.count=0;
       var folders={};      
@@ -254,11 +327,10 @@ function getData(aData){
     }catch(e){}
     return obj;
   }    
-  if(!this.mobile){
-    var reg=new RegExp("\"Address\":\""+this.user.replace(/@/,"\\\\u0040")+"\"","i");
-    var fnd2=aData.match(reg);
-    if(!fnd2)return obj;
-  }
+
+  var reg=new RegExp("\"Address\":\""+this.user.replace(/@/,"\\\\u0040")+"\"","i");
+  var fnd2=aData.match(reg);
+  if(!fnd2)return obj;
 
   if(this.isNew==1){
     var fnd=aData.match(/HM.ContainerPoolData\(.+?\{([\s\S]+?)\},\s*\[([\S\s]+?)\]/);
@@ -273,10 +345,10 @@ function getData(aData){
         if(!this.includeSpam&&(o[1]=="fljunk"||o[2]=="fljunk"))continue;
         var n=parseInt(o[4]);
         if(o[1]=="fljunk"||o[2]=="fljunk"){
-              if(this.includeSpam==2)this.count+=n;
-            }else if(this.inboxOnly){
+          if(this.includeSpam==2)this.count+=n;
+        }else if(this.inboxOnly){
           if(o[1]=="flinbox")this.count+=n;
-            }else this.count+=n;
+        }else this.count+=n;
         if(n>0&&o[1]!="flinbox"){
           var name=o[3];
           name=unescape(name.replace(/\\u/g,"%u").replace(/\\x/g,"%"));
@@ -296,43 +368,19 @@ function getData(aData){
       }
       return obj;
     }
-  }else if(!this.mobile){
-    var fnd=aData.match(/<ul.+?class="List FolderList.+?>([\s\S]+?)<\/ul>/g);
-    if(fnd){
-      fnd=fnd.toString();
-      var re=/<li.+?\s+id="(.+?([^-]+?))"\s+nm="(.+?)"\s+count="(\d+)"/g;
-      var o;
-      this.count=0;
-      while ((o = re.exec(fnd)) != null){
-        if(o[2]=="000000000004"
-          ||o[2]=="000000000003"||o[2]=="000000000002")continue;
-        if(!this.includeSpam&&o[2]=="000000000005")continue;
-        var n=parseInt(o[4]);
-        if(o[2]=="000000000005"){
-          if(this.includeSpam==2)this.count+=n;
-        }else if(this.inboxOnly){
-          if(o[2]=="000000000001")this.count+=n;
-        }else this.count+=n;
-        if(n>0&&o[2]!="000000000001"){
-          var name=o[3];
-          name=name.replace(/&#(\d+);/g,function(){return String.fromCharCode(RegExp.$1);});
-          ar.push({id:o[1].replace(/-/g,""),title:name,count:n});
-        }
-      }
-      if(this.showFolders){
-        if(ar)obj.folders=ar;
-      }
-      return obj;
-    }
-  }else{
-    var re=/<td><a.+?href=".+?fid=(.+?([^-]+?))"\s+>(.+?)(?:\s\((\d+)\))?<\/a>/g;
+  }
+
+  var fnd=aData.match(/<ul.+?class="List FolderList.+?>([\s\S]+?)<\/ul>/g);
+  if(fnd){
+    fnd=fnd.toString();
+    var re=/<li.+?\s+id="(.+?([^-]+?))"\s+nm="(.+?)"\s+count="(\d+)"/g;
     var o;
     this.count=0;
-    while ((o = re.exec(aData)) != null){
+    while ((o = re.exec(fnd)) != null){
       if(o[2]=="000000000004"
         ||o[2]=="000000000003"||o[2]=="000000000002")continue;
       if(!this.includeSpam&&o[2]=="000000000005")continue;
-      var n=o[4]?parseInt(o[4]):0;
+      var n=parseInt(o[4]);
       if(o[2]=="000000000005"){
         if(this.includeSpam==2)this.count+=n;
       }else if(this.inboxOnly){
@@ -349,14 +397,12 @@ function getData(aData){
     }
     return obj;
   }
-  this.count=-1;
   return obj;
 }
 function getViewURL(aFolder){
   if(aFolder&&this.mailHost){
     if(this.isNew==2)return this.mailHost+"/"+encodeURIComponent(aFolder);
     else if(this.isNew==1)return this.mailHost+"/?fid="+aFolder;
-    else if(this.mobile)return this.mailHost+"/m/folders.m?fid="+aFolder;
     var url=this.mailHost+"/mail/InboxLight.aspx?fid="+aFolder
               +"&n="+parseInt(Math.random()*1000000000);
     return url;
