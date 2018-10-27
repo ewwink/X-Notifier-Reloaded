19cdeef0cafbe366190fa3c25dc0619f9fd568da
Jeroen <xnassist@gmail.com>
2018-10-27 21:02:48 +0200

    app_forums.js

--- components/scripts/app_forums.js
+++ components/scripts/app_forums.js
@@ -3,6 +3,7 @@ XN-forums
   @require lib-update.js
 ***********************************************************/
 var name="XN-forums";
+var ver="2018-07-13";
 var defaultInterval=120;
 
 function init(){
@@ -10,16 +11,35 @@ function init(){
   if(this.user=="default"){
     this.noCookie=true;
   }else{
-    this.initStage=ST_LOGIN;
+    this.initStage=ST_PRE;
     this.loginData=["http://xnotifier.tobwithu.com/dp/forum?destination=forum","name","pass","op=Log+in&form_id=user_login_block"];
   }
 
   this.dataURL="http://xnotifier.tobwithu.com/dp/forum/";
-  this.viewURL="http://xnotifier.tobwithu.com/dp/"+(this.user=="default"?"forum/1":"tracker");
+  this.viewURL="http://xnotifier.tobwithu.com/dp/"+(this.user=="default"?"forum/2":"tracker");
   this.viewDomain="xnotifier.tobwithu.com";
   this.start="<div id=\"forum\">[\\s\\S]+?<tbody>";
   this.end="<\/tbody>";
 }
+function process(aData,aHttp) {
+if(this.debug)dlog(this.id+"\t"+this.user+"\t"+this.stage,aData);
+  switch(this.stage){
+  case ST_PRE:
+    this.getHtml("http://xnotifier.tobwithu.com/dp/");
+    return false;
+  case ST_PRE_RES:
+    var fnd=aData.match(/id="user-login-form"/);  
+    if(fnd){
+      this.stage=ST_LOGIN;      
+      var fnd2=aData.match(/"antibot".+?"key":"(\S+?)"/);
+      if(fnd2){
+        this.getHtml(this.loginData[LOGIN_URL],this.loginData[LOGIN_POST]+"&antibot_key="+encodeURIComponent(fnd2[1]));
+        return false;
+      }
+    }else this.stage=ST_LOGIN_RES;
+  }
+  return this.baseProcess(aData,aHttp);
+}    
 function findString(aData){
   if(this.user!="default"){
     var fnd=aData.match(/<input.+?id="edit-submit".+?value="Log in"/);
